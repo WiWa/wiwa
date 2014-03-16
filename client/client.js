44a574ad6ceeb8eacd9985455e917aab39b2fc34
allidoiswin <fallenpwr@gmail.com>
2014-03-16 00:51:09 -0400

    design overhaul, will add image + about + actual projects

--- client/client.js
+++ client/client.js
@@ -1,43 +1,31 @@
 if (Meteor.isClient) {
 
-  var views = ["navHome", "navAbout", "navProjects", "navResume"];
+  var views = ["navHome", "navProjects"];
 
 
-  Template.headerT.events({
+  Template.viewMain.events({
     'click #navHome' : function () {
 	Session.set("view", 0);
 	$("#divHome").css({"display":"inline"});
 	$(".view[id!=divHome]").hide();
-	console.log(Session.get("view"))
+	//console.log(Session.get("view"))
+	
     },
-
-    /*'click #navAbout' : function () {
-	Session.set("view", 1);
-	$("#divAbout").css({"display":"inline"});
-	$(".view[id!=divAbout]").hide();
-	console.log(Session.get("view"))
-    },*/
 
     'click #navProjects' : function () {
-	Session.set("view", 2);
+	Session.set("view", 1);
 	$("#divProjects").css({"display":"inline"});
 	$(".view[id!=divProjects]").hide();
-	console.log(Session.get("view"))
+	//console.log(Session.get("view"))
     },
 
-    /*'click #navResume' : function () {
-	Session.set("view", 3);
-	$("#divResume").css({"display":"inline"});
-	$(".view[id!=divResume]").hide();
-	console.log(Session.get("view"))
-    },*/
 
     'click li' : function () {
 	for(var i = 0; i < views.length; i++){
 
 	  if(Session.get("view") == i){
 	    $("#"+views[i] ).css({
-	    	"box-shadow": "0 5px 2px -1px rgba(0, 255, 255, 0.76)"
+	    	"box-shadow": "0 5px 2px -1px rgba(0, 255, 255, 0.95)"
 	    });
 	  }
 	  else{
@@ -54,25 +42,18 @@ if (Meteor.isClient) {
   Template.viewMain.home = function () {
 	return Session.get("view")==0;
   }
-  Template.viewMain.about = function () {
-	return Session.get("view")==1;
-  }
   Template.viewMain.projects = function () {
-	return Session.get("view")==2;
-  }
-  Template.viewMain.resume = function () {
-	return Session.get("view")==3;
+	return Session.get("view")==1;
   }
 
 
 
   Session.set("view", 0);
   Template.viewMain.rendered = function () {
-	//$("#divAbout").hide();
 	$("#divProjects").hide();
-	//$("#divResume").hide();
-	console.log("rendered");
-
+	$("#navHome").click();
+	
+	//Dynamic Project List
 	Meteor.subscribe("projects", function(e, r){
 		Projects = new Meteor.Collection("projects");
 		var pArr = Projects.find().fetch();
@@ -81,6 +62,32 @@ if (Meteor.isClient) {
 			$("#divProjects").append(p);
 		}
 	});
+	styleHome();
   }
 
 }
+
+function styleHome(){
+	///Set up parts
+	var a = $($('#divIntro p')[0]);
+	var b = $($('#divIntro p')[1]);
+	var c = $($('#divIntro p')[2]);
+	var d = $($('#divIntro p')[3]);
+	var e = $($('#divIntro p')[4]);
+	var f = $($('#divIntro p')[5]);
+
+	$('#divIntro').animate({"margin-top":"140px"},500);
+	$('#divIntro p').animate({"margin-top":"40px"},500, function(){
+		a.animate({"font-size":"4.1em", "margin-bottom":"-5px", "margin-left":"-30px"}, 800);
+		b.animate({"font-size":"1.8em", "margin-bottom":"-20px"}, 700);
+		c.animate({"font-size":"1.8em", "margin-bottom":"-20px", "margin-left":"80px"}, 700);
+		d.animate({"font-size":"1.8em", "margin-bottom":"-20px" }, 700);
+		e.animate({"font-size":"1.8em", "margin-bottom":"-12px", "margin-left":"60px"}, 700);
+		f.animate({"font-size":"3.1em", "margin-top":"52px", "margin-left":"30px"}, 700);
+	});
+
+	
+}
+
+
+
