ff49810a052bb1af6a82463ddc5f88a272138db1
allidoiswin <fallenpwr@gmail.com>
2014-03-16 01:50:31 -0400

    projects page looks nice, will add links to social media, about

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ if (Meteor.isServer) {
     // code to run on server at startup
 	Projects = new Meteor.Collection("projects");
 
-	//Projects.remove({});
+	Projects.remove({});
 	if(!Projects.find().count()){
-		Projects.insert({name:"First Project", description:"Hello World"});
+		Projects.insert({image:"https://raw.githubusercontent.com/stuycs-softdev-2012-2013/WWEB/master/ett/templates/icon.png", name:"Babble", description:"'Language Learning Through Your Browser' is the idea behind this Chrome Extension I helped create for a software development course.", link:"https://github.com/stuycs-softdev-2012-2013/WWEB"});
 	}
 
 	Meteor.publish("projects", function(){
