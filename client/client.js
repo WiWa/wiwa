b0d8696556916a9c5fbd2652fcc2c503c0f42ffc
allidoiswin <fallenpwr@gmail.com>
2014-03-17 04:32:21 -0400

    lots  of overhauls to html/css, blurb. Zoom is beauts now. next up: typography.

--- client/client.js
+++ client/client.js
@@ -2,27 +2,23 @@ if (Meteor.isClient) {
 
   var views = ["navHome", "navProjects"];
 
-
+////Specified in events.js
   Template.viewMain.events({
+//////// On clicking a tab, hide other views, show relevant view
     'click #navHome' : function () {
 	Session.set("view", 0);
 	$("#divHome").css({"display":"inline"});
 	$(".view[id!=divHome]").hide();
-	//console.log(Session.get("view"))
-	
     },
-
     'click #navProjects' : function () {
 	Session.set("view", 1);
 	$("#divProjects").css({"display":"inline"});
 	$(".view[id!=divProjects]").hide();
-	//console.log(Session.get("view"))
     },
 
-
+/////// On clicking li (a tab), do boxshadow (underlining) stuff
     'click li' : function () {
 	for(var i = 0; i < views.length; i++){
-
 	  if(Session.get("view") == i){
 	    $("#"+views[i] ).css({
 	    	"box-shadow": "0 5px 2px -2px rgba(0, 205, 255, 0.95)"
@@ -33,26 +29,25 @@ if (Meteor.isClient) {
 	    	"box-shadow": ""
 	    });
 	  }
-
 	}
     },
-
+//////// On clicking More About Me!, remove cool styling, show the about
     'click #ablbl' : function (){
 	normalHome();
     }
 
   });
 
+////Tab functionality
   Template.viewMain.home = function () {
 	return Session.get("view")==0;
   }
   Template.viewMain.projects = function () {
 	return Session.get("view")==1;
   }
-
-
-
+////Default to Home
   Session.set("view", 0);
+////On page load
   Template.viewMain.rendered = function () {
 	$("#divProjects").hide();
 	$("#navHome").click();
@@ -67,7 +62,6 @@ if (Meteor.isClient) {
 		}
 	});
 	styleHome();
-		console.log($("#and")[0]);
   }
 
 }
@@ -78,18 +72,14 @@ function styleHome(){
 	var b = $($('#divIntro p')[1]);
 	var c = $($('#divIntro p')[2]);
 	var d = $($('#divIntro p')[3]);
-	var e = $($('#divIntro p')[4]);
-	var f = $($('#divIntro p')[5]);
 	
 	$('#divIntro p').css({"margin-top":"40px"});
 	$('#divIntro').animate({"margin-top":"30px"},600,function(){
 		$("#and").animate({"top":"5px"}, 400);})
-		a.css({"font-size":"4.1em", "margin-bottom":"-5px", "margin-left":"-30px"}, 700);
-		b.css({"font-size":"1.8em", "margin-bottom":"-20px"}, 700);
-		c.css({"font-size":"1.8em", "margin-bottom":"-20px", "margin-left":"80px"}, 700);
-		d.css({"font-size":"1.8em", "margin-bottom":"-20px" }, 700);
-		e.css({"font-size":"1.8em", "margin-bottom":"-12px", "margin-left":"60px"}, 700);
-		f.css({"font-size":"3.1em", "margin-top":"52px", "margin-left":"30px"});
+		a.css({"font-size":"3.6em", "margin-bottom":"-5px", "margin-left":"-30px"}, 700);
+		b.css({"font-size":"1.5em", "margin-bottom":"-20px"}, 700);
+		c.css({"font-size":"1.5em", "margin-bottom":"-20px" }, 700);
+		d.css({"font-size":"2.7em", "margin-left":"-2px"});
 }
 function normalHome(){
 	$("#ablbl").hide();
@@ -97,23 +87,19 @@ function normalHome(){
 	var b = $($('#divIntro p')[1]);
 	var c = $($('#divIntro p')[2]);
 	var d = $($('#divIntro p')[3]);
-	var e = $($('#divIntro p')[4]);
-	var f = $($('#divIntro p')[5]);
 	a.animate({"color":"black", "font-size":"1.1em", "margin-bottom":"0px", "margin-left":"0px"}, 500);
 	b.animate({"font-size":"1.1em", "margin-bottom":"0px"}, 500);
-	c.animate({"font-size":"1.1em", "margin-bottom":"0px", "margin-left":"0px"}, 500);
-	d.animate({"font-size":"1.1em", "margin-bottom":"0px" }, 500);
-	e.animate({"font-size":"1.1em", "margin-bottom":"0px", "margin-left":"0px"}, 500);
-	f.animate({"color":"black", "font-size":"1.1em", "margin-top":"0px", "margin-left":"0px"},500);
+	c.animate({"font-size":"1.1em", "margin-bottom":"0px" }, 500);
+	d.animate({"color":"black", "font-size":"1.1em", "margin-top":"0px", "margin-left":"0px"},500);
 	$('#divIntro p').css({"text-shadow":"none"});
-	$('#divIntro p').animate({"margin-top":"0px"},260,function(){$("#divIntro").animate({"margin-top":"0px","margin-left":"36px"},260);$('#divMore').show(500);});
+	$('#divIntro p').animate({"margin-top":"0px"},260,function(){
+	    $("#divIntro").animate({"margin-top":"0"},260);
+	    $('#divMore').show(500);});
 	
 	a.text("Win Wang");
-	b.text("Gamer");
-	c.text("Student");
-	d.text("Physicist");
-	e.text("Computer Scientist");
-	f.text("");
+	b.text("Student");
+	c.text("Physicist");
+	d.text("Computer Scientist");
 }
 
 
